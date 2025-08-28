2f284b82034b63c3b5433f0056c61f5ddb26ca3d
Julee Anjana <julieanjana2002@gmail.com>
2025-08-28 11:47:55 +0530

    Create My Bookings page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
+import MyBooking from "./pages/MyBookings";
 
 const App = () => {
 
@@ -18,6 +19,7 @@ const App = () => {
         <Route path ='/' element={<Home/>} />
         <Route path ='/rooms' element={<AllRooms/>} />
         <Route path ='/rooms/:id' element={<RoomDetails/>} />
+        <Route path ='/My-bookings' element={<MyBooking/>} />
       </Routes>
       </div>
       <Footer />
